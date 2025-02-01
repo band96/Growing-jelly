dc87eae5126a942f0d8fbac5f009805ef7718b29
band96 <bandskt@naver.com>
2025-02-02 00:36:14 +0900

    0.10
    클리커 기능 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Vector3[] pointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
+
 }
